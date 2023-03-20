dc338beab8661480676aaa851a07cafb8fc563ce
Justin Keowen <jkeowen@gmail.com>
2023-03-20 16:39:50 -0500

    finished builiding db

--- db/seed.js
+++ db/seed.js
@@ -1,13 +1,19 @@
 const {
     client, 
     createUser, 
-    createActivity
+    createActivity, 
+    createRoutine,
+    createExerciseType, 
+    assignActivityToRoutine
 } = require('./db');
 
 const dropTables = async() =>{
     await client.query(`
         DROP TABLE IF EXISTS users;
+        DROP TABLE IF EXISTS routines_activities;
         DROP TABLE IF EXISTS activities;
+        DROP TABLE IF EXISTS routines;
+        DROP TABLE IF EXISTS exercise_type;
     `)
 };
 
@@ -18,16 +24,29 @@ const buildTables = async() =>{
                             last_name VARCHAR(25) NOT NULL,
                             username VARCHAR(25) UNIQUE NOT NULL,
                             password VARCHAR(25) UNIQUE NOT NULL,
-                            age INT NOT NULL,
-                            weight INT NOT NULL,
-                            email_address VARCHAR(25) NOT NULL);
+                            age INTEGER NOT NULL,
+                            weight INTEGER NOT NULL,
+                            email_address VARCHAR(25) NOT NULL,
+                            is_active BOOLEAN DEFAULT TRUE);
+        CREATE TABLE exercise_type( id SERIAL PRIMARY KEY,
+                                    name VARCHAR(25) NOT NULL UNIQUE);
+        CREATE TABLE routines( id SERIAL PRIMARY KEY,
+                                creator_id INTEGER DEFAULT '0',
+                                name VARCHAR(25),
+                                description TEXT NOT NULL,
+                                type_id INTEGER REFERENCES exercise_type(id),
+                                is_public BOOLEAN DEFAULT 'true',
+                                is_active BOOLEAN DEFAULT 'true');
         CREATE TABLE activities( id SERIAL PRIMARY KEY,
-                                 name VARCHAR(25) UNIQUE NOT NULL,
-                                 instructions TEXT NOT NULL,
-                                 reps INT NOT NULL,
-                                 sets INT NOT NULL,
-                                 equipment VARCHAR(25) NOT NULL,
-                                 description TEXT NOT NULL);
+                                    name VARCHAR(25) UNIQUE NOT NULL,
+                                    instructions TEXT NOT NULL,
+                                    reps INTEGER NOT NULL,
+                                    sets INTEGER NOT NULL,
+                                    equipment VARCHAR(25) NOT NULL,
+                                    type_id INTEGER REFERENCES exercise_type(id),
+                                    description TEXT NOT NULL);
+        CREATE TABLE routines_activities(id_routines INTEGER REFERENCES routines(id),
+                                          id_activities INTEGER REFERENCES activities(id));
     `)
 }
 
@@ -44,13 +63,34 @@ const createUsers = async() =>{
     console.log('CREATING USERS');
     await createUser('Pubs', 'Studly', 'heavyLifter25', 'Lift4Gains4Lyf', '32', '180', 'pStuds@exerciz.com')
     console.log('FINISHED CREATING USERS');
-}
+};
 
 const createActivities = async() =>{
     console.log('CREATING ACTIVITIES')
-    await createActivity('Bench Press', 'LIFT THE IRON PLACEHOLDER', '10', '5', 'Bench, Barbell', 'Uhh its a bench press' )
+    await createActivity('Bench Press', 'LIFT THE IRON PLACEHOLDER', '10', '5', 'Bench, Barbell', 2, 'Uhh its a bench press');
+    await createActivity('Sprints', 'Run fast for a short distance', '5', '5', 'Track, feet', 1, 'Run real fast, be explosive, focus on technique');
     console.log('FINISHED CREATING ACTIVITIES')
+};
 
+const createExerciseTypes = async() =>{
+    console.log("CREATING EXERCISE TYPES");
+    await createExerciseType('Cardio');
+    await createExerciseType('Pumping Iron');
+    console.log("FINISHED CREATING EXERCISE TYPES");
+}
+
+const createRoutines = async() =>{
+    console.log('CREATING ROUTINES');
+    await createRoutine(0, 'SUPER CARDIO EXTREME', 'THIS WILL MAKE YOUR GD HEART EXPLODE', 1, true, true);
+    await createRoutine(0, 'GET RIPPED', "LIFT SOME HEAVY STUFF", 2, true, true);
+    console.log('FINISHED CREATING ROUTINES');
+}
+
+const assignActivityToRoutines = async() =>{
+    console.log("ASSIGNING ACTIVITIES TO ROUTINES");
+    await assignActivityToRoutine(1,1);
+    await assignActivityToRoutine(2,2);
+    console.log('FINISHED ASSIGNING ACTIVITIES TO ROUTINES');
 }
 
 const testDb = async()=>{
@@ -59,7 +99,10 @@ const testDb = async()=>{
     console.log("FINISHED CONNECTING");
     await rebuildTables();
     await createUsers();
+    await createExerciseTypes();
     await createActivities();
+    await createRoutines();
+    await assignActivityToRoutines();
     console.log('DISCONNECTING FROM DB');
     client.end();
     console.log('FINISHED DISCONNECTING FROM DB');

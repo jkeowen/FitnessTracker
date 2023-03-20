6449c163430a0bcb057b5e2bc5f156d653a13366
Justin Keowen <jkeowen@gmail.com>
2023-03-20 14:03:11 -0500

    started setting up db

--- db/db.js
+++ db/db.js
@@ -1,1 +1,9 @@
-// hi justin :)
+const { Client } = require('pg');
+const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/fitness_tracker-dev');
+
+console.log('CONNECTING TO DB')
+client.connect();
+console.log('FINISHED CONNECTING')
+console.log('DISCONNECTING')
+client.end();
+console.log('FINISHED DICONNECTING')

35adac80530af1009f102fe60885d0ea5e0bcd1d
Justin Keowen <jkeowen@gmail.com>
2023-03-20 14:45:14 -0500

    part of db built

--- db/db.js
+++ db/db.js
@@ -1,9 +1,27 @@
 const { Client } = require('pg');
 const client = new Client(process.env.DATABASE_URL || 'postgres://localhost:5432/fitness_tracker-dev');
 
-console.log('CONNECTING TO DB')
-client.connect();
-console.log('FINISHED CONNECTING')
-console.log('DISCONNECTING')
-client.end();
-console.log('FINISHED DICONNECTING')
+const createUser = async(firstName, lastName, username, password, age, weight, emailAddress) =>{
+
+    const { rows: [ user ] } = await client.query(`
+        INSERT INTO users(first_name, last_name, username, password, age, weight, email_address)
+        VALUES($1, $2, $3, $4, $5, $6, $7)
+        RETURNING *;
+    `, [firstName, lastName, username, password, age, weight, emailAddress]);
+    return user;
+};
+
+const createActivity = async(name, instructions, reps, sets, equipment, descritpion) =>{
+    const { rows: [ activity ] } = await client.query(`
+        INSERT INTO activities(name, instructions, reps, sets, equipment, description)
+        VALUES($1, $2, $3, $4, $5, $6)
+        RETURNING *;
+    `, [name, instructions, reps, sets, equipment, descritpion]);
+    return activity;
+}
+
+module.exports={
+    client,
+    createUser,
+    createActivity
+}

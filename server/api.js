5fe4f04327c99813ccdc773f0c0682fb9f444959
Justin Keowen <jkeowen@gmail.com>
2023-03-27 11:33:15 -0500

    started jwt nonsense

--- server/api.js
+++ server/api.js
@@ -8,8 +8,37 @@ const exerciseTypeRouter = require('./exerciseType');
 const routinesActivitiesRouter = require('./routinesActivities');
 const usersActivitiesRouter = require('./usersActivities');
 const usersRoutinesRouter = require('./usersRoutines');
+const jwt = require('jsonwebtoken');
+
+const  { getSingleUser } = require('../db');
+const { JWT_SECRET } = process.env;
 
 apiRouter.use(express.json()); 
+
+apiRouter.use(async(req, res, next)=>{
+  const prefix = 'Bearer ';
+  const auth = req.header('Authorization');
+
+  if(!auth){
+    next();
+  }else if(auth.startsWith(prefix)){
+    const token = auth.slice(prefix.length);
+    try{
+      const { id } = jwt.verify(token, JWT_SECRET);
+      if(id){
+        req.user = await getSingleUser(id);
+        next();
+      }
+    }catch({name, message}){
+      next({name, message});
+    }
+  }else{
+    next({
+      name: 'AuthorizationHeaderError',
+      message: `Authorization token must start with ${ prefix }`
+    })
+  }
+})
 
 apiRouter.use('/users', userRouter);
 apiRouter.use('/activities', activitiesRouter);

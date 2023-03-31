054710810aa841c78ac5f85b6161ebfb43adae78
Callie Cassan <contactccassan@gmail.com>
2023-03-31 10:59:37 -0500

    computer is tweaking :(

--- src/components/Register.js
+++ src/components/Register.js
@@ -11,12 +11,17 @@ const [username, setUsername] = useState('');
 const [password, setPassword] = useState('');
 const [confirmPassword, setConfirmPassword] = useState('');
 
-// const handleChange = (event) => {
-//   if(placeholder === "First Name") {setFirstName(event.target.value)
-//   }else if (event.target.placeholder === ) {
-
-//   }
-// } 
+const handleChange = (event) => {
+  if(event.target.placeholder === "First Name") setFirstName(event.target.value);
+  else if (event.target.placeholder === "Last Name" ) setLastName(event.target.value);
+  else if (event.target.placeholder === "Userame" ) setUsername(event.target.value);
+  else if (event.target.placeholder === "Password" ) setPassword(event.target.value);
+  else if (event.target.placeholder === "Confirm Password" ) setConfirmPassword(event.target.value);
+  else if (event.target.placeholder === "Age" ) setAge(event.target.value);
+  else if (event.target.placeholder === "Weight" ) setPassword(event.target.value);
+  else setEmail(event.target.value)
+}
+console.log(firstName, lastName)
 
 const handleSubmit = async (event) => {
   event.preventDefault()
@@ -30,14 +35,14 @@ const handleSubmit = async (event) => {
           Register Here
         </header>
         <form onSubmit= {handleSubmit}>
-        <input placeholder = "First Name"/>
-        <input placeholder = "Last Name"/>
-        <input placeholder="Username" />
-        <input placeholder="Password" />
-        <input placeholder = "Confirm Password"/>
-        <input placeholder = "Age"/>
-        <input placeholder = "Weight"/>
-        <input placeholder = "Email Address"/>
+        <input onClick = {handleChange} placeholder = "First Name"/>
+        <input onClick = {handleChange} placeholder = "Last Name"/>
+        <input onClick = {handleChange} placeholder="Username" />
+        <input onClick = {handleChange} placeholder="Password" />
+        <input onClick = {handleChange} placeholder = "Confirm Password"/>
+        <input onClick = {handleChange} placeholder = "Age"/>
+        <input onClick = {handleChange} placeholder = "Weight"/>
+        <input onClick = {handleChange} placeholder = "Email Address"/>
         <button type= 'submit'>Submit</button>
         </form>
       </div>
